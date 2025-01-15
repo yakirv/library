11b1a75204c16258be7d8942184f7af8a98dc0af
Yakirv <yakirv@gmail.com>
2025-01-15 22:39:27 +0200

    refactoring js functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const myLibrary = [{'BookName': 'Harry Potter', 'author':'j.k Rowling', 'pages':'1,250'},{'BookName': 'Song of ice and fire', 'author':'j.r.r Martin', 'pages':'2,550'}];
+/* const myLibrary = [{'BookName': 'Harry Potter', 'author':'j.k Rowling', 'pages':'1,250'},{'BookName': 'Song of ice and fire', 'author':'j.r.r Martin', 'pages':'2,550'}];
+ */ 
+const myLibrary = [];
 
 
 function Book(BookName, author, pages) {
@@ -13,59 +15,63 @@ function addBookToLibrary(BookName, author, pages) {
     let newBook = new Book(BookName, author,pages)
     myLibrary.push(newBook)
   }
+function createNewBookCardElement()
+{
+    const libraryContainer = document.getElementById('library-container');
+    const bookElement = document.getElementById("books-container");
+    libraryContainer.removeChild(bookElement);
+    const newEl = document.createElement('div');
+    newEl.id = 'books-container';
+    libraryContainer.appendChild(newEl);
+};
+function addBookCard(actionType){myLibrary.forEach(book => {
+    const bookElement = document.createElement('div');
+    bookElement.className = 'book-card'
+    bookElement.textContent = `The title is ${book.BookName} by ${book.author}, have ${book.pages} pages`;
+    if (actionType == 1 )
+    {
+        booksContainer.appendChild(bookElement);
+    }
+    else if (actionType == 2)
+    {
+        const newEl = document.getElementById('books-container');
+        newEl.appendChild(bookElement);
+    }
+});} 
 
 
-// ---------------Show the books in the array---------------
 const showButton = document.getElementById("show-button");
-const updateButton = document.getElementById("update-button");
 const mainContent = document.getElementById("main-content");
 const libraryContainer = document.getElementById('library-container');
 const booksContainer = document.getElementById('books-container');
 const emptyLibrary = document.getElementById('emptyMessage');
+
+// ---------------Show the books in the array---------------
 showButton.addEventListener('click', function(){
-    libraryContainer.style.display = 'grid';
-    emptyLibrary.style.display = 'none';
-    updateButton.style.display = 'inline-block';
-  function addBookCard(){myLibrary.forEach(book => {
-        const bookElement = document.createElement('div');
-        bookElement.className = 'book-card'
-        bookElement.textContent = `The title is ${book.BookName} by ${book.author}, have ${book.pages} pages`;
-        booksContainer.appendChild(bookElement);
-    });} 
-   addBookCard()
-})
-// ---------------Refresh the book lists---------------
-
-updateButton.addEventListener('click', function()
-{  
-     const libraryContainer = document.getElementById('library-container');
-    const bookElement = document.getElementById("books-container");
-    if(bookElement)
+    if(myLibrary.length === 0)
     {
-       // updateButton.innerHTML = 'success';
-        libraryContainer.removeChild(bookElement);
-        console.log(bookElement)
-        const newEl = document.createElement('div');
-        //newEl.className ='books-container';
-        newEl.id = 'books-container';
-       libraryContainer.appendChild(newEl);
-       
-        function addBookCard(){myLibrary.forEach(book => {
-            const bookElement = document.createElement('div');
-            bookElement.className = 'book-card'
-            bookElement.textContent = `The title is ${book.BookName} by ${book.author}, have ${book.pages} pages`;
-            newEl.appendChild(bookElement);
-        });}
-        addBookCard();
-      
-    } 
-   
+        emptyLibrary.innerText = 'Still Empty';
+    }else
+    {
+        libraryContainer.style.display = 'grid';
+        emptyLibrary.style.display = 'none';
+    }
     
+   
+
+   addBookCard(1)
 })
 
+function cleanForm()
+{
+    const CleanBookTitle = document.getElementById("title").value = '' ;
+    const CleanBookAuthor = document.getElementById("author").value = '';
+    const CleanBookpPages = document.getElementById("pages").value = '';
+}
 
 
 // ---------------open the new book form---------------
+
 const AddButton = document.getElementById("add-button"); 
 const formContainer = document.getElementById("form-container");
 AddButton.addEventListener('click', function()
@@ -81,11 +87,11 @@ AddButton.addEventListener('click', function()
         formContainer.style.display ='none';
         AddButton.style.display = 'flex';
     }
-    
-    
+     
 })
 
 // ---------------Adding new book---------------
+
 const confirmButton = document.getElementById("confirm");
 confirmButton.addEventListener('click',function()
 {
@@ -94,10 +100,13 @@ confirmButton.addEventListener('click',function()
     const newBookAuthor = document.getElementById("author").value;
     const newBookpPages = document.getElementById("pages").value;
     addBookToLibrary(newBookTitle,newBookAuthor,newBookpPages);
-  //addBookCard();
     formContainer.style.display ='none' ;
     AddButton.style.display = 'block';
-    const CleanBookTitle = document.getElementById("title").value = '' ;
-    const CleanBookAuthor = document.getElementById("author").value = '';
-    const CleanBookpPages = document.getElementById("pages").value = '';
+   
+    cleanForm();
+    createNewBookCardElement();
+    emptyLibrary.style.display = 'none';
+    libraryContainer.style.display = 'grid';
+    addBookCard(2);
+  
 });
